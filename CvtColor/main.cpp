80b8457e5cf6c5e78a7366385229ddc6e9a53bc6
ShayHQ <shayizaki@gmail.com>
2023-01-11 17:51:50 +0200

    * Added white balance function (fixed values were taken from Yifat's imageDisplayEnhance.ini)

--- CvtColor/main.cpp
+++ CvtColor/main.cpp
@@ -8,12 +8,38 @@
 #include <iostream>
 
 cv::Mat cvtColorRGGB(cv::Mat& img);
+void wbImage(cv::Mat& img, double r = 0, double g = 0, double b = 0);
+
 
 int main(void){
     cv::Mat sampleImg = cv::imread("/home/shay/tmp/testing-white-img.png", cv::IMREAD_UNCHANGED); // MONOCHROME
     cv::Mat colorImg = cvtColorRGGB(sampleImg);
+    wbImage(colorImg, 0.835, 0, 0.608);
     cv::imwrite("MyCVTRes.png", colorImg);
     return 0;
+}
+
+void wbImage(cv::Mat& img, double r, double g, double b){
+    std::vector<cv::Mat> bgr(3);
+    std::vector<double> wb {b,g,r};
+    
+    cv::split(img, bgr);
+    for (cv::Mat& ch : bgr){
+        ch.convertTo(ch, CV_32FC1);
+    }
+    for (int chIdx = 0 ; chIdx < bgr.size() ; chIdx++){
+        cv::Mat& ch = bgr[chIdx];
+        for (int i = 0 ; i < ch.rows; i ++){
+            for (int j = 0; j < ch.cols ; j ++){
+                ch.ptr<float>(i)[j] *= (1 + wb[chIdx]);
+            }
+        }
+    }
+    for (cv::Mat& ch : bgr){
+        ch.convertTo(ch, CV_8UC1);
+    }
+
+    cv::merge(bgr.data(), bgr.size(), img);
 }
 
 cv::Mat cvtColorRGGB(cv::Mat& img){
@@ -43,6 +69,8 @@ cv::Mat cvtColorRGGB(cv::Mat& img){
     }
 
     cv::Mat g = cv::max(g1, g2);
+    
+    // linear opencv interpolation
     cv::resize(r, r, colorImg.size(), 0, 0, cv::INTER_LINEAR);
     cv::resize(g, g, colorImg.size(), 0, 0, cv::INTER_LINEAR);
     cv::resize(b, b, colorImg.size(), 0, 0, cv::INTER_LINEAR);
