09d009b4f2f38a50e91a1ca3488c63852ae5411e
ShayHQ <shayizaki@gmail.com>
2023-01-17 21:36:32 +0200

    * calculating WB correction values without ROI
    Using the green channel as a reference is a common technique used in white balance correction because the green channel is less affected by the color temperature of the lighting compared to the blue and red channels. By using the green channel as a reference, the correction values are calculated such that the green channel remains unchanged, while the blue and red channels are adjusted to match the green channel's color balance.

---
+++ PresetWB/main.cpp
@@ -0,0 +1,47 @@
+
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include "opencv2/imgproc.hpp"
+#include <vector>
+#include <iostream>
+
+cv::Vec2d genWB(cv::Mat& whiteColor);
+
+int main(void){
+    cv::Mat testingWBImage = cv::imread("./whiteCalibImage.png", cv::IMREAD_UNCHANGED); // MONOCHROME
+    cv::cvtColor(testingWBImage, testingWBImage, cv::COLOR_BayerBG2BGR);
+
+    double expectedRFactor = 1.4495851453792237;
+    double expectedBFactor = 0.47584357278590383;
+    auto wb = genWB(testingWBImage);
+
+    assert((float)wb[0] == (float)expectedBFactor);
+    assert((float)wb[1] == (float)expectedRFactor);
+
+    return 0;
+}
+
+/*
+ This function calculates the correction values for the blue and red 
+ channels of the image by comparing the means of these channels to the mean of the 
+ green channel, which is less affected by the color temperature of the lighting.
+*/
+cv::Vec2d genWB(cv::Mat& whiteColor){
+    cv::Vec2d wb { 0, 0 };
+
+    cv::Mat bgr[3];
+    cv::split(whiteColor, bgr);
+
+    // In perfect white image color gMean will peak value of 255
+    double gMean = cv::mean(bgr[1])[0]; // mean of the green channel is less affected by the color temperature of the lighting.
+    double bMean = cv::mean(bgr[0])[0];
+    double rMean = cv::mean(bgr[2])[0];
+
+    wb[0] = gMean / bMean - 1;
+    wb[1] = gMean / rMean - 1;
+
+    return wb;
+}
+
